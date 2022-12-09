2c05400a016f89ef8d621a54cd9f0fe3e4a36153
Vadim <opizah70@gmail.com>
2022-12-09 16:42:57 +0500

    dz

---
+++ Dz seminar 3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+if (a.Length == 5)
+{
+    if (a[1] == a[4] && a[2] == a[3])
+    {
+        Console.WriteLine("Число " + a + " Являеться Полиндромом ");
+    }
+    else
+    {
+        Console.WriteLine("Число не явялеться полиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
---
+++ dz 23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0;i< n;i++)
+{
+    Console.Write($"{Math.Pow(i, 3)},"); 
+}
+
+
